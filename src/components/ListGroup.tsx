2a3d661f2ca073b13bad0dc311fbf56044f30b44
keith japitana <keithjapitana1996@gmail.com>
2024-05-04 03:16:02 +0800

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,18 @@
+const ListGroup = () => {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
