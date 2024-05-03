2e1a31b1431f3f457abc2af3f824dda1f372c799
keith japitana <keithjapitana1996@gmail.com>
2024-05-04 04:13:44 +0800

    Implementing List group.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 const ListGroup = () => {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List Group</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group ">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index), console.log(selectedIndex);
+            }}
+          >
             {item}
           </li>
         ))}
