c9e8d976f52a6bd10855d8c78e25011a59da487d
keith japitana <keithjapitana1996@gmail.com>
2024-05-21 13:39:07 +0800

    implementing form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ListGroup from "./components/ListGroup";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "./components/Form";
+import "./index.css";
 
 const App = () => {
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <Form />
     </div>
   );
 };
